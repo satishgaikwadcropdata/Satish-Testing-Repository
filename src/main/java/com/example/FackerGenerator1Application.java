18529a9cd7df257714fbf5dfd93e27fe2a59709f
satishgaikwadcropdata <satish.gaikwad@cropdata.in>
2023-04-22 12:39:58 +0530

    first commit

---
+++ src/main/java/com/example/FackerGenerator1Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FackerGenerator1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FackerGenerator1Application.class, args);
+
+		System.out.println("---------------------Wllcome----------------");
+	}
+
+}
